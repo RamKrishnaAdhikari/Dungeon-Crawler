21fc030d5a614d0bd06230d3af635d3225403816
Saugat Pandey <saugat.pandey@stud.h-da.de>
2024-05-17 19:34:38 +0000

    Upload New File

---
+++ Praktikum 2/floor.h
@@ -0,0 +1,12 @@
+#ifndef FLOOR_H
+#define FLOOR_H
+#include "tile.h"
+
+class Floor : public Tile
+{
+ public:
+    Floor(const std::string& texture, int row, int column);
+    std::pair<bool, Tile*> onEnter(Character* who) override;
+};
+
+#endif // FLOOR_H
