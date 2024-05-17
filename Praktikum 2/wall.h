2f298e054fc781682f5ca3d04ca8e8dcc601d193
Saugat Pandey <saugat.pandey@stud.h-da.de>
2024-05-17 19:35:12 +0000

    Upload New File

---
+++ Praktikum 2/wall.h
@@ -0,0 +1,13 @@
+#ifndef WALL_H
+#define WALL_H
+#include "tile.h"
+
+class Wall : public Tile
+{
+ public:
+    Wall(const std::string& texture, int row, int column);
+    std::pair<bool, Tile*> onEnter(Character* who) override;
+};
+
+#endif // WALL_H
+
