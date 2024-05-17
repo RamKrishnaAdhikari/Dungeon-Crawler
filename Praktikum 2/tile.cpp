7d8e79121688b25b6ebd5a229e956e4498c0c04e
Saugat Pandey <saugat.pandey@stud.h-da.de>
2024-05-17 19:34:18 +0000

    Upload New File

---
+++ Praktikum 2/tile.cpp
@@ -0,0 +1,92 @@
+#include "tile.h"
+#include "character.h"
+
+Tile::Tile(const std::string& texture, int row, int column) : texture(texture),
+    character(nullptr),
+    row(row),
+    column(column)
+{}
+
+void Tile::setTexture(const std::string& newTexture)
+{
+    texture = newTexture;
+}
+
+void Tile::setCharacter(Character* newCharacter)
+{
+    character = newCharacter;
+}
+
+Character* Tile::getCharacter() const
+{
+    return character;
+}
+
+int Tile::getRow() const
+{
+    return row;
+}
+
+int Tile::getColumn() const
+{
+    return column;
+}
+
+std::string Tile::getTexture() const
+{
+    if (character != nullptr)
+    {
+        return character->getTexture();
+    }
+    else
+    {
+        return texture;
+    }
+}
+
+bool Tile::hasCharacter() const
+{
+    if (character != nullptr)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+bool Tile::moveTo(Tile* destTile, Character* who)
+{
+    if (this->onLeave(destTile, who))
+    {
+        if (destTile->onEnter(who).first)
+        {
+            if (destTile->onEnter(who).second == nullptr)
+            {
+                destTile->setCharacter(who);
+                this->setCharacter(nullptr);
+                who->setCurrentTile(destTile);
+                return true;
+            }
+            else
+            {
+                Tile* portalDestination = destTile->onEnter(who).second;
+                portalDestination->setCharacter(who);
+                this->setCharacter(nullptr);
+                who->setCurrentTile(portalDestination);
+                return true;
+            }
+        }
+    }
+    return false;
+}
+
+bool Tile::onLeave(Tile* destTile, Character* who)
+{
+    return true;
+}
+
+
+
+
