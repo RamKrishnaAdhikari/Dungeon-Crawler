5afa36bce8fb80d6e16afe07bad8bf94bc0efb3c
Ram Krishna Adhikari <ramkrishna.adhikari@stud.h-da.de>
2024-05-31 14:06:38 +0000

    Upload New File

---
+++ Praktikum 3/portal.cpp
@@ -0,0 +1,14 @@
+#include "portal.h"
+
+Portal::Portal(const std::string& texture, int row, int column) : Tile(texture, row, column), destination(nullptr)
+{}
+
+void Portal::setDestination(Portal* newDestination)
+{
+    destination = newDestination;
+}
+
+std::pair<bool, Tile*> Portal::onEnter(Character* who)
+{
+    return {true, destination};
+}
